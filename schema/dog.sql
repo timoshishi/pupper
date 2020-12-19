533c70a178224b4ae3efec3a3f345741567dcb5c
timoshishi <timfrrst@gmail.com>
2020-12-19 18:01:30 -0500

    Data gen

--- schema/dog.sql
+++ schema/dog.sql
@@ -1,11 +1,9 @@
 CREATE TABLE IF NOT EXISTS dog(
   id serial primary key,
   user_id varchar,
+  name varchar(100),
   breed varchar(100),
   color varchar(100),
   adult_weight int,
   age int,
-  CONSTRAINT fk_user
-  FOREIGN KEY(user_id)
-    REFERENCES users(user_id)
 );
