1ec4328ff80aa3c45bfb699137450ea3b9ae7f81
timoshishi <timfrrst@gmail.com>
2020-12-24 15:54:07 -0500

    Implement client side auth and protected routes with React Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Switch, Route, withRouter } from 'react-router-dom';
+import { NavBar, Footer, Loading } from './components';
+import { Home, Profile, ExternalApi } from './views';
+import ProtectedRoute from './auth/protected-route';
 
-function App() {
+import { Auth0Provider, useAuth0 } from '@auth0/auth0-react';
+
+function App(props) {
+  const { isLoading } = useAuth0();
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id='app' className='d-flex flex-column h-100'>
+      <NavBar />
+      <div className='container flex-grow-1'>
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <ProtectedRoute path='/profile' component={Profile} />
+          <ProtectedRoute path='/external-api' component={ExternalApi} />
+        </Switch>
+      </div>
+      <Footer />
     </div>
   );
 }
 
-export default App;
+export default withRouter(App);
+// import { AuthConfig } from 'react-use-auth';
+// import { Auth0 } from 'react-use-auth/auth0';
+// import Home from './components/Home';
+// import AUTHCallback from './components/AUTHCallback';
+
+// <AuthConfig
+//   authProvider={Auth0}
+//   navigate={props.history.push}
+//   params={{
+//     domain: process.env.REACT_APP_AUTH_DOMAIN,
+//     clientID: process.env.REACT_APP_AUTH_CLIENT_ID,
+// }}>
+// </AuthConfig>
--- index.js
+++ index.js
@@ -6,12 +6,13 @@ console.log(process.env.DATABASE_URL);
 app.use(require('morgan')('dev'));
 app.use(express.json({ extended: false }));
 
-app.use(express.static(path.join(__dirname, 'client/build')));
-app.use('/api/users', require('./routes/users'));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use('/api/users', require('./routes/users'));
 
-app.get('*', (req, res) =>
-  res.sendFile(path.join(`${__dirname}/client/build/index.html`))
-);
-
+  app.get('*', (req, res) =>
+    res.sendFile(path.join(`${__dirname}/client/build/index.html`))
+  );
+}
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
