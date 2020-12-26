bb40cef5108195a2bd0e0d9682377fc74e66f455
timoshishi <timfrrst@gmail.com>
2020-12-26 12:15:21 -0500

    Remove Profile

--- client/src/views/index.js
+++ client/src/views/index.js
@@ -1,6 +1,6 @@
 import ExternalApi from './ExternalApi';
 import Home from './Home';
 import Landing from './Landing';
-import Profile from './Profile';
+// import Profile from './Profile';
 
-export { ExternalApi, Home, Landing, Profile };
+export { ExternalApi, Home, Landing };
