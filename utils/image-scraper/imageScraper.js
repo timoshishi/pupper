117baf8914a7f84c3646785b32e0fed510e64cf1
timoshishi <timfrrst@gmail.com>
2020-12-30 11:55:59 -0500

    Update createDog.js, imageScraper.js

--- utils/image-scraper/imageScraper.js
+++ utils/image-scraper/imageScraper.js
@@ -8,7 +8,6 @@ const dogBreeds = [
   'pug',
   'shitzu',
   'bulldog',
-  'bloodhound',
   'poodle',
   'spaniel',
   'shiba',
@@ -17,7 +16,6 @@ const dogBreeds = [
   'chihuahua',
   'husky',
   'dachshund',
-  'great dane',
   'maltese',
   'pomerian',
   'corgi',
@@ -33,32 +31,34 @@ const imageScraper = async () => {
   const path = `${__dirname}\\example.png`;
   try {
     dogBreeds.forEach(async (dog) => {
-      const browser = await puppeteer.launch();
-      const page = await browser.newPage();
+      setTimeout(async () => {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
 
-      const puppy = `https://source.unsplash.com/800x600/?${dog},puppy`;
-      await page.goto(puppy);
-      await page.on('load');
-      await page.screenshot({ path: 'example.png' });
+        const puppy = `https://source.unsplash.com/800x600/?${dog}`;
+        await page.goto(puppy);
+        await page.on('load');
+        await page.screenshot({ path: 'example.png' });
 
-      //add file to form data for sending to server
-      const formData = new FormData();
-      formData.append('image', fs.createReadStream(path));
+        //add file to form data for sending to server
+        const formData = new FormData();
+        formData.append('image', fs.createReadStream(path));
 
-      const options = {
-        method: 'POST',
-        body: formData,
-        headers: {
-          Authorization: `Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImNfT2JBTmNkTlpNT0RlZGhEWndHdyJ9.eyJpc3MiOiJodHRwczovL2Rldi1pdXhpaGQ0NS51cy5hdXRoMC5jb20vIiwic3ViIjoiZ29vZ2xlLW9hdXRoMnwxMTE2Nzk2MzkwMzkxODEwMjI2MDUiLCJhdWQiOlsiaHR0cHM6Ly90aW1vc2hpc2hpLXB1cHByLmhlcm9rdWFwcC5jb20vIiwiaHR0cHM6Ly9kZXYtaXV4aWhkNDUudXMuYXV0aDAuY29tL3VzZXJpbmZvIl0sImlhdCI6MTYwOTI2NTIxNywiZXhwIjoxNjA5MzUxNjE3LCJhenAiOiJRcnBVRjM1N0hLRFFQdUw3YmQ5MEdTbTZHY3dYeVhCUSIsInNjb3BlIjoib3BlbmlkIHByb2ZpbGUgZW1haWwifQ.crgrlKCvtKUBGgnIRvOkfx-1dTdMgrLV1vi_xw5HgLw135lpu-QPf3ssEbziCH8scL8krgtQlRh3T0R_zbMmF9pNaicNN_lJZ5bm8sa3LTFgPRvimZ9jMkF98LQYF0PDvqAlXk8HroNKy29DdAtU3GDgoByu3Txeu6MVPn8HZjK6j1GatJOu1hKekzlZfBQc_28qEOev1Q63fcLZlogc0MDtei5-k00t-Vj2InAQ02l7y7wBW9Wj3PVJi3QUEZjbJB_S7s3IlElpVvD7v9Dd87mMbLZMeZfzAEUuMyRlhG7KxHUekQwgQndXCqehMpBqLKtJklKjJZMO7KIvfQbkKQ`,
-        },
-      };
-      const response = await fetch(
-        `http://localhost:5000/api/users/photos/${dog}`,
-        options
-      );
-      const url = await response.json();
-      console.log('url.msg', url.msg);
-      await browser.close();
+        const options = {
+          method: 'POST',
+          body: formData,
+          headers: {
+            Authorization: `Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImNfT2JBTmNkTlpNT0RlZGhEWndHdyJ9.eyJpc3MiOiJodHRwczovL2Rldi1pdXhpaGQ0NS51cy5hdXRoMC5jb20vIiwic3ViIjoiZ29vZ2xlLW9hdXRoMnwxMTE2Nzk2MzkwMzkxODEwMjI2MDUiLCJhdWQiOlsiaHR0cHM6Ly90aW1vc2hpc2hpLXB1cHByLmhlcm9rdWFwcC5jb20vIiwiaHR0cHM6Ly9kZXYtaXV4aWhkNDUudXMuYXV0aDAuY29tL3VzZXJpbmZvIl0sImlhdCI6MTYwOTI2NTIxNywiZXhwIjoxNjA5MzUxNjE3LCJhenAiOiJRcnBVRjM1N0hLRFFQdUw3YmQ5MEdTbTZHY3dYeVhCUSIsInNjb3BlIjoib3BlbmlkIHByb2ZpbGUgZW1haWwifQ.crgrlKCvtKUBGgnIRvOkfx-1dTdMgrLV1vi_xw5HgLw135lpu-QPf3ssEbziCH8scL8krgtQlRh3T0R_zbMmF9pNaicNN_lJZ5bm8sa3LTFgPRvimZ9jMkF98LQYF0PDvqAlXk8HroNKy29DdAtU3GDgoByu3Txeu6MVPn8HZjK6j1GatJOu1hKekzlZfBQc_28qEOev1Q63fcLZlogc0MDtei5-k00t-Vj2InAQ02l7y7wBW9Wj3PVJi3QUEZjbJB_S7s3IlElpVvD7v9Dd87mMbLZMeZfzAEUuMyRlhG7KxHUekQwgQndXCqehMpBqLKtJklKjJZMO7KIvfQbkKQ`,
+          },
+        };
+        const response = await fetch(
+          `http://localhost:5000/api/users/photos/${dog}`,
+          options
+        );
+        const url = await response.json();
+        console.log('url.msg', url.msg);
+        await browser.close();
+      }, 500);
     });
   } catch (err) {
     return console.error('@imageScraper', err.message);
